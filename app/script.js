8d623420e9c696f6ba13fea3c6ae2415e83f3478
nickveil <31191433+nickveil@users.noreply.github.com>
2018-01-13 17:19:11 -0500

    WIP one column running, spacing issue between names and container growth incorrect

--- app/script.js
+++ app/script.js
@@ -1,1 +1,46 @@
-var grid = new Muuri('.grid',{dragEnabled:true});
+var itemContainers = [].slice.call(document.querySelectorAll('.board-col-content'));
+var columnGrids = [];
+var boardGrid;
+
+itemContainers.forEach(function (container){
+	var grid = new Muuri(container,{
+		items: '.board-team-member',
+		layoutDuration:400,
+		layoutEasing: 'ease',
+		dragEnabled:true,
+		dragSort: function(){
+			return columnGrids;
+		},
+		dragSortInterval:0,
+		dragContainer: document.body,
+		dragReleaseDuration: 400,
+		dragReleaseEasing: 'ease'
+		})
+		.on('dragStart', function(item){
+			item.getElement().style.width = item.getWidth() + 'px';
+			item.getElement().style.height = item.getHeight() + 'px';
+		})
+		.on('dragReleaseEnd', function (item){
+			item.getElement().style.width = '';
+    	item.getElement().style.height = '';
+    	columnGrids.forEach(function(grid){
+    		grid.refreshItems();
+    	});
+		})
+		.on('layoutStart', function(){
+			boardGrid.refreshItems().layout();
+		})
+		columnGrids.push(grid);
+});
+
+boardGrid = new Muuri('.board', {
+  layoutDuration: 400,
+  layoutEasing: 'ease',
+  dragEnabled: true,
+  dragSortInterval: 0,
+  dragStartPredicate: {
+    handle: '.board-col-header'
+  },
+  dragReleaseDuration: 400,
+  dragReleaseEasing: 'ease'
+});
